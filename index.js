26320c8249daaa92e1dd91e8758265c1d088ebb5
BerdibekUalov <uralov0212@gmail.com>
2023-06-02 19:16:52 +0500

    learn express.js

--- index.js
+++ index.js
@@ -1,5 +1,20 @@
-import express from "express"
+import express from "express";
+import path, {dirname} from 'path';
+import { fileURLToPath } from "url";
 
-const app = express()
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = dirname(__filename)
+
+const app = express();
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'views', 'index.html'))
+})
+
+app.get('/about', (req, res) => {
+  res.status(200)
+  res.sendFile(path.join(__dirname, 'views', 'about.html'))
+})
+
 const PORT = process.env.PORT || 4444
 app.listen(PORT, () => console.log(`Server is running on port: ${PORT}`))
